82e48cd69f0fa9e4ec58a1570033f3e81d34122c
chigov2 <chigov@gmail.com>
2021-11-19 13:21:56 +0200

    9.34

--- app/src/main/java/com/chigov/gueststar/MainActivity.java
+++ app/src/main/java/com/chigov/gueststar/MainActivity.java
@@ -2,13 +2,103 @@ package com.chigov.gueststar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private ImageView imageStar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        imageStar = findViewById(R.id.imageStar);
+    }
+
+    //создаем два класса - для загрузки изображений и контента
+    private static class DownloadContentTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                //открываем соединение
+                urlConnection = (HttpURLConnection) url.openConnection();
+                //получаем поток ввода
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                //начинаем читать построчно
+                String line = reader.readLine();
+                //будем читать до тех пор пока строка не будет равна нулю
+                while (line != null) {
+                    result.append(line);
+                    line = reader.readLine();
+                }
+                //когда чтение будет закончено возвращаем
+                return result.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                //открываем соединение
+                urlConnection = (HttpURLConnection) url.openConnection();
+                //получаем поток ввода
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
     }
 }
